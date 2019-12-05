f9df4c15f63086691c06eebf05109f5252b24a28
Seelan <norwell.sigwebela@prodigyfinance.com>
2019-12-05 14:27:35 +0200

    Updates

---
+++ precache-manifest.994e34e30bd360414a196495fff2fd31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90c5d9169fae060a7cf3600318334e5",
+    "url": "/monsters-roldex/index.html"
+  },
+  {
+    "revision": "358aedd07d3354cc8620",
+    "url": "/monsters-roldex/static/css/main.3dd31aa2.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters-roldex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "358aedd07d3354cc8620",
+    "url": "/monsters-roldex/static/js/main.89f85e66.chunk.js"
+  },
+  {
+    "revision": "8e7ff660c163bbdd3a9a",
+    "url": "/monsters-roldex/static/js/runtime-main.8bb729de.js"
+  }
+]);
